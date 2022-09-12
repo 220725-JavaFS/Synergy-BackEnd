69306ca22a16c555beb53704c5a38a4746204ba1
Kira Smiley <kira640@revature.net>
2022-09-12 18:49:02 -0400

    added spring security dependency

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,18 +1,15 @@
 package com.revature.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.models.Users;
 import com.revature.services.UserService;
 
@@ -30,7 +27,7 @@ public class UserController {
 	}
 	
 	//for viewing info persistence from front to back end
-	@GetMapping("register")
+	@GetMapping("users")
 	public ResponseEntity<List<Users>> getAllUsers() {
 		List<Users> users = us.findAllUsers();
 		return ResponseEntity.status(HttpStatus.OK).body(users);
